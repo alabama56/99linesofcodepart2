c81ac4fae31127c279091117b7c2610630d95498
alabama56 <31999279+alabama56@users.noreply.github.com>
2017-09-29 08:46:31 -0500

    attempeting to complete the extra credit with for loobs for each div and divbutton

--- extracredit.js
+++ extracredit.js
@@ -7,8 +7,10 @@ var button = document.createElement("button");
 var text = document.createTextNode("Sing!");
 var divButton = document.createElement("button");
 var friendDiv = document.createElement("div");
+friendDiv.setAttribute("class", "friend");
 
 document.addEventListener("DOMContentLoaded", function(){
+
 
 
     button.appendChild(text);
@@ -18,7 +20,9 @@ document.addEventListener("DOMContentLoaded", function(){
     
     button.addEventListener("click", function(){
         var divButton = document.createElement("button");
-        
+
+    
+
   
         for(var i=0; i <friends.length; i++ ){
             var friendDiv = document.createElement("div");
@@ -32,20 +36,33 @@ document.addEventListener("DOMContentLoaded", function(){
             friendDiv.appendChild(divHeader);
             friendDiv.appendChild(divButton);
             document.body.appendChild(friendDiv);
-            
-            divButton.addEventListener("click", onButtonClick.bind(this));
-              
+
+            divButton.addEventListener("click", buttonPress)    
+           
+            function buttonPress(name){ 
+                for(var j = 99; j>0; j--){
+                    var para = document.createElement("p");
+                    friendDiv.appendChild(para);
+                    if (j === 2) {
+                        var song = document.createTextNode(j + " lines of code in the file " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file");
+                    para.appendChild(song);
+                    } else if (j === 1) {
+                        var song = document.createTextNode(j + " lines of code in the file " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+                    para.appendChild(song);
+                    } else {
+                        var song = document.createTextNode(j + " lines of code in the file " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
+                    para.appendChild(song);
+                    }
+                }    
+            }
         }
-
         
-    
-    });
 
-    
+
+
+    }); 
+
+
+
 });
 
-
-
-function onButtonClick(){
-    console.log("help");
-}
