927902d8feb17f7f5cdd3f6625305b460b8bca5a
alabama56 <jakedownard@yahoo.com>
2017-09-29 09:38:09 -0500

    final push with the 'extra credit'

--- extracredit.js
+++ extracredit.js
@@ -37,35 +37,26 @@ document.addEventListener("DOMContentLoaded", function(){
             friendDiv.appendChild(divButton);
             document.body.appendChild(friendDiv);
             
-            divButton.addEventListener("click", buttonPress.bind(friends[i]))    
-           
-   
+            divButton.addEventListener("click", buttonPress.bind(null, friends[i], friendDiv))      
         }
-        
-
-
-
     }); 
-
-
-
 });
 
-function buttonPress(name){ 
-    for(var i=0; i <friends.length; i++ ){
-        for(var j = 99; j>0; j--){
-            var para = document.createElement("p");
-            friendDiv.appendChild(para);
-            if (j === 2) {
-                var song = document.createTextNode(j + " lines of code in the file " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file");
-            para.appendChild(song);
-            } else if (j === 1) {
-                var song = document.createTextNode(j + " lines of code in the file " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
-            para.appendChild(song);
-            } else {
-                var song = document.createTextNode(j + " lines of code in the file " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
-            para.appendChild(song);
-            }
-        }   
-    }     
+function buttonPress(name, friendDiv){ 
+    
+    for(var j = 99; j>0; j--){
+        var para = document.createElement("p");
+        friendDiv.appendChild(para);
+        if (j === 2) {
+            var song = document.createTextNode(j + " lines of code in the file " + j + " lines of code; " + name + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file");
+        para.appendChild(song);
+        } else if (j === 1) {
+            var song = document.createTextNode(j + " lines of code in the file " + j + " lines of code; " + name + " strikes one out, clears it all out, no more lines of code in the file");
+        para.appendChild(song);
+        } else {
+            var song = document.createTextNode(j + " lines of code in the file " + j + " lines of code; " + name + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
+        para.appendChild(song);
+        }
+    }   
+         
 }
